810ccd0f3966ca7584a739423d56b0b0925d9238
Jan Blömacher <Jan@Bloemacher.com>
2020-05-30 16:47:36 +0200

    Added MahApps Metro

--- Songify/Forms/MainWindow.xaml.cs
+++ Songify/Forms/MainWindow.xaml.cs
@@ -12,13 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro;
 
 namespace Songify
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
