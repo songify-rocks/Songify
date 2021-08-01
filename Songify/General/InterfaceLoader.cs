cc805e3b7698bbf2afd18bfa4ded4020963690d1
Cyklan <dev@cyklan.de>
2021-08-01 18:15:24 +0200

    Added plugin loader

---
+++ Songify/General/InterfaceLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Songify.General
+{
+    internal static class InterfaceLoader
+    {
+        public static T Get<T>()
+        {
+            return GetAll<T>().FirstOrDefault();
+        }
+
+        public static List<T> GetAll<T>()
+        {
+            List<T> interfaces = new();
+
+            GetDlls().ForEach(d =>
+            {
+                try
+                {
+                    foreach (Type t in Assembly.LoadFrom(d).GetTypes())
+                    {
+                        if (typeof(T) == t) { continue; }
+                        if (t.GetInterface(typeof(T).FullName) is null) { continue; }
+
+                        interfaces.Add((T)Activator.CreateInstance(t));
+                    }
+                }
+                catch { }
+            });
+
+            return interfaces;
+        }
+
+        private static List<string> GetDlls()
+        {
+            return Directory.GetFiles(PathManager.PluginDirectory).Where(x => x.EndsWith(".dll")).ToList();
+        }
+    }
+}
