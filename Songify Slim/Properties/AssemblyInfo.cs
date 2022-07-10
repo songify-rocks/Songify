e7681d337c509b4415b0540853e6a0f4cfec8613
Inzaniity <Jan@Bloemacher.com>
2022-07-10 16:37:00 +0200

    Raised version number to 1.3.4

--- Songify Slim/Properties/AssemblyInfo.cs
+++ Songify Slim/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.3")]
-[assembly: AssemblyFileVersion("1.3.3")]
+[assembly: AssemblyFileVersion("1.3.4")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: Guid("442379e3-32d8-42d1-ab09-cba229672453")] 
