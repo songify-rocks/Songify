3344de0ddb8572da65a243e433f5598d09f9070d
Inzaniity <Jan@Bloemacher.com>
2023-03-24 18:52:41 +0100

    Reading config files now ignores unknown fields

--- Songify Slim/Util/Settings/ConfigHandler.cs
+++ Songify Slim/Util/Settings/ConfigHandler.cs
@@ -68,6 +68,7 @@ namespace Songify_Slim.Util.Settings
             string path = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
             var deserializer = new DeserializerBuilder()
                 .WithNamingConvention(CamelCaseNamingConvention.Instance)  // see height_in_inches in sample yml
+                .IgnoreUnmatchedProperties()
                 .Build();
 
             Configuration config = new Configuration();
