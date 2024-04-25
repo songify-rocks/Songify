46c3302c57e9d40ad5dbf7827fb38032ac931c06
Jan Blömacher <jan@bloemacher.com>
2024-04-25 23:34:01 +0200

    changed version to rc_5

--- Songify Slim/Views/MainWindow.xaml.cs
+++ Songify Slim/Views/MainWindow.xaml.cs
@@ -522,7 +522,7 @@ namespace Songify_Slim.Views
             cbx_Source.SelectionChanged += Cbx_Source_SelectionChanged;
 
             // text in the bottom right
-            LblCopyright.Content = GlobalObjects.IsBeta ? $"Songify v1.5.4.rc_4 Copyright ©" : $"Songify v{GlobalObjects.AppVersion} Copyright ©";
+            LblCopyright.Content = GlobalObjects.IsBeta ? $"Songify v1.5.4.rc_5 Copyright ©" : $"Songify v{GlobalObjects.AppVersion} Copyright ©";
 
             if (_selectedSource == PlayerType.SpotifyWeb)
             {
