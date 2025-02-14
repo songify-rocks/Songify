7f570d58ad73734ebc916825e66497baf961a017
Jan Blömacher <jan@bloemacher.com>
2025-02-15 00:15:28 +0100

    Reformat classes and update constructor in GeneralModels.cs
    Reformatted classes for consistent indentation and readability.
    Updated DeleteTrackUri constructor to use property initializers.
    No functional changes to code logic or behavior.

--- Songify Slim/Util/Spotify/SpotifyAPI.Web/Models/GeneralModels.cs
+++ Songify Slim/Util/Spotify/SpotifyAPI.Web/Models/GeneralModels.cs
@@ -5,154 +5,148 @@ using Newtonsoft.Json;
 
 namespace Songify_Slim.Util.Spotify.SpotifyAPI.Web.Models
 {
-  public class Image
-  {
-    [JsonProperty("url")]
-    public string Url { get; set; }
+    public class Image
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
 
-    [JsonProperty("width")]
-    public double Width { get; set; }
+        [JsonProperty("width")]
+        public double Width { get; set; }
 
-    [JsonProperty("height")]
-    public double Height { get; set; }
-  }
+        [JsonProperty("height")]
+        public double Height { get; set; }
+    }
 
-  public class ErrorResponse : BasicModel
-  { }
+    public class ErrorResponse : BasicModel
+    { }
 
-  public class Error
-  {
-    [JsonProperty("status")]
-    public int Status { get; set; }
+    public class Error
+    {
+        [JsonProperty("status")]
+        public int Status { get; set; }
 
-    [JsonProperty("message")]
-    public string Message { get; set; }
-  }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
 
-  public class PlaylistTrackCollection
-  {
-    [JsonProperty("href")]
-    public string Href { get; set; }
+    public class PlaylistTrackCollection
+    {
+        [JsonProperty("href")]
+        public string Href { get; set; }
 
-    [JsonProperty("total")]
-    public int Total { get; set; }
-  }
+        [JsonProperty("total")]
+        public int Total { get; set; }
+    }
 
-  public class Followers
-  {
-    [JsonProperty("href")]
-    public string Href { get; set; }
+    public class Followers
+    {
+        [JsonProperty("href")]
+        public string Href { get; set; }
 
-    [JsonProperty("total")]
-    public int Total { get; set; }
-  }
+        [JsonProperty("total")]
+        public int Total { get; set; }
+    }
 
-  public class PlaylistTrack
-  {
-    [JsonProperty("added_at")]
-    public DateTime AddedAt { get; set; }
+    public class PlaylistTrack
+    {
+        [JsonProperty("added_at")]
+        public DateTime AddedAt { get; set; }
 
-    [JsonProperty("added_by")]
-    public PublicProfile AddedBy { get; set; }
+        [JsonProperty("added_by")]
+        public PublicProfile AddedBy { get; set; }
 
-    [JsonProperty("track")]
-    public FullTrack Track { get; set; }
+        [JsonProperty("track")]
+        public FullTrack Track { get; set; }
 
-    [JsonProperty("is_local")]
-    public bool IsLocal { get; set; }
-  }
+        [JsonProperty("is_local")]
+        public bool IsLocal { get; set; }
+    }
 
-  public class DeleteTrackUri
-  {
     /// <summary>
     ///     Delete-Track Wrapper
     /// </summary>
     /// <param name="uri">An Spotify-URI</param>
     /// <param name="positions">Optional positions</param>
-    public DeleteTrackUri(string uri, params int[] positions)
+    public class DeleteTrackUri(string uri, params int[] positions)
     {
-      Positions = positions.ToList();
-      Uri = uri;
+        [JsonProperty("uri")]
+        public string Uri { get; set; } = uri;
+
+        [JsonProperty("positions")]
+        public List<int> Positions { get; set; } = [.. positions];
+
+        public bool ShouldSerializePositions()
+        {
+            return Positions.Count > 0;
+        }
     }
 
-    [JsonProperty("uri")]
-    public string Uri { get; set; }
-
-    [JsonProperty("positions")]
-    public List<int> Positions { get; set; }
-
-    public bool ShouldSerializePositions()
+    public class Copyright
     {
-      return Positions.Count > 0;
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
     }
-  }
 
-  public class Copyright
-  {
-    [JsonProperty("text")]
-    public string Text { get; set; }
+    public class LinkedFrom
+    {
+        [JsonProperty("external_urls")]
+        public Dictionary<string, string> ExternalUrls { get; set; }
 
-    [JsonProperty("type")]
-    public string Type { get; set; }
-  }
+        [JsonProperty("href")]
+        public string Href { get; set; }
 
-  public class LinkedFrom
-  {
-    [JsonProperty("external_urls")]
-    public Dictionary<string, string> ExternalUrls { get; set; }
+        [JsonProperty("id")]
+        public string Id { get; set; }
 
-    [JsonProperty("href")]
-    public string Href { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
 
-    [JsonProperty("id")]
-    public string Id { get; set; }
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+    }
 
-    [JsonProperty("type")]
-    public string Type { get; set; }
+    public class SavedTrack
+    {
+        [JsonProperty("added_at")]
+        public DateTime AddedAt { get; set; }
 
-    [JsonProperty("uri")]
-    public string Uri { get; set; }
-  }
+        [JsonProperty("track")]
+        public FullTrack Track { get; set; }
+    }
 
-  public class SavedTrack
-  {
-    [JsonProperty("added_at")]
-    public DateTime AddedAt { get; set; }
+    public class SavedAlbum
+    {
+        [JsonProperty("added_at")]
+        public DateTime AddedAt { get; set; }
 
-    [JsonProperty("track")]
-    public FullTrack Track { get; set; }
-  }
+        [JsonProperty("album")]
+        public FullAlbum Album { get; set; }
+    }
 
-  public class SavedAlbum
-  {
-    [JsonProperty("added_at")]
-    public DateTime AddedAt { get; set; }
+    public class Cursor
+    {
+        [JsonProperty("after")]
+        public string After { get; set; }
 
-    [JsonProperty("album")]
-    public FullAlbum Album { get; set; }
-  }
+        [JsonProperty("before")]
+        public string Before { get; set; }
+    }
 
-  public class Cursor
-  {
-    [JsonProperty("after")]
-    public string After { get; set; }
+    public class Context
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
 
-    [JsonProperty("before")]
-    public string Before { get; set; }
-  }
+        [JsonProperty("href")]
+        public string Href { get; set; }
 
-  public class Context
-  {
-    [JsonProperty("type")]
-    public string Type { get; set; }
+        [JsonProperty("external_urls")]
+        public Dictionary<string, string> ExternalUrls { get; set; }
 
-    [JsonProperty("href")]
-    public string Href { get; set; }
-
-    [JsonProperty("external_urls")]
-    public Dictionary<string, string> ExternalUrls { get; set; }
-
-    [JsonProperty("uri")]
-    public string Uri { get; set; }
-  }
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+    }
 }
